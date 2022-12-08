0f322799395048d0883e5b8f3419a3cf03809539
Russell James <russelljames202@gmail.com>
2022-12-07 20:53:44 -0500

    Add task.js file
    Handles the creation of all task objects of a project through a factory
    function.

---
+++ src/js/task.js
@@ -0,0 +1,12 @@
+export default task;
+
+function task(title,
+             description,
+             dueDate,
+             priority,
+             project){
+
+    return {title, description, dueDate, priority, project}
+}
+
+
