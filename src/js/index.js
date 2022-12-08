2815e05dc1da6f60088c273f93b32319a5fa4df4
Russell James <russelljames202@gmail.com>
2022-12-07 20:44:10 -0500

    Add index JS file
    Handles UI rendering and event listeners.

---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+import '../style.css'
+import {default as renderUI} from './renderUI';
+import {default as addEventListeners} from './listeners';
+
+/**
+ * Commit
+ * Work on creating local storage for user
+ * Style page, commit often
+ * push project
+ */
+
+renderUI();
+addEventListeners();
